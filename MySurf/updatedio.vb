9e0ad59d9109888c7de42bf5784c89d6df084d44
AJ <aj@emposoft.tk>
2014-10-24 21:15:55 -0400

    Clearing space for Alpha 3

--- MySurf/updatedio.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports System.Net
-
-Public Class updatedio
-
-    Private Sub update_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        WebBrowser1.Navigate("http://mysurf.tk/downloads/update-details.txt")
-    End Sub
-End Class
