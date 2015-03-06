33134ae5e950f552a3cd35228b2b560f34670ef6
AJ <aj@emposoft.com>
2015-03-06 11:05:18 -0500

    Making room for MySurf with Chromium 39
    The new source should be up in the next few days.

--- MySurf/theme.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class theme
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Me.Hide()
-        main.Show()
-        main.ChromeWebBrowser1.OpenUrl("http://themes.mysurf.tk/")
-    End Sub
-End Class
